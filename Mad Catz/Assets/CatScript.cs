7f4fc3a3408e9beac051270704c6b781de098a11
Mikko Ilmonen <miq.ilmonen@gmail.com>
2015-11-22 00:48:00 +0200

    A lot of models and some scripts

--- Mad Catz/Assets/CatScript.cs
+++ Mad Catz/Assets/CatScript.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class CatScript : MonoBehaviour {
 
     SoundLevel SoundLevel;
+
+    public AudioSource meowSource;
+    public AudioClip[] meowMix;
+
+    TimeSpan meowInterval = TimeSpan.FromMilliseconds(3000);
+    float meowRandomness = 0.3f;
+    DateTime nextMeow;
 
     void OnCollisionEnter(Collision collision)
     {
@@ -16,10 +24,16 @@ public class CatScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         SoundLevel = GameObject.Find("SoundLevelMonitor").GetComponent<SoundLevel>();
-	}
+        nextMeow = DateTime.Now.AddMilliseconds(UnityEngine.Random.Range(500, 8000));
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        DateTime now = DateTime.Now;
+	    if (now > nextMeow)
+        {
+            nextMeow = now.AddMilliseconds(UnityEngine.Random.Range(0,100000));
+            meowSource.PlayOneShot(meowMix[UnityEngine.Random.Range(0,meowMix.Length)]);
+        }
 	}
 }
